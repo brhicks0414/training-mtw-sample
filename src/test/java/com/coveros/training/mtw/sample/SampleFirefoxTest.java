feca4e4cf2a1ef2aee7bcbd8a74d5a03dfafd391
brian <brian.hicks@coveros.com>
2016-04-29 10:00:17 -0400

    Initial commit of sample test class

---
+++ src/test/java/com/coveros/training/mtw/sample/SampleFirefoxTest.java
@@ -0,0 +1,39 @@
+package com.coveros.training.mtw.sample;
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Simple test class that verifies proper installation of MTW tools and libraries
+ * 
+ * @author brian
+ *
+ */
+public class SampleFirefoxTest {
+	private WebDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	/**
+	 * Runs a simple test verifying the title of the amazon.com homepage.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void amazon() throws Exception {
+		driver.get("http://www.amazon.com/");
+		assertEquals("Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more",
+				driver.getTitle());
+	}
+}
